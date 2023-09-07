17cdd66a9d8aaaf704442691c6ab64349353955d
2024.zelan.eroz.espanto@uwcisak.jp <ZelanISAK>
2023-09-07 20:14:04 +0900

    75 uploaded

--- Quizzes/src/75_texttobits.py
+++ Quizzes/src/75_texttobits.py
@@ -1,16 +1,16 @@
 class pizza():
     def text_to_bits(text:str):
-        temp, output = "",""
+        temp, binary = "",""
         for i in text:
             i=ord(i)
             while i>0:
                 temp=str(i%2)+temp
                 i=i//2
-            temp+=" "
-            if len(temp)!=8:
-                temp+="0"*(8-len(temp))
-            #output+=temp[::-1] Only works in Python so always make algorithms that can work in any lang.
-            output+=temp
-        return output
+            if len(temp)<8:
+                temp=("0"*(8-len(temp)))+temp
+            #output+=temp[::-1]
+            binary+=temp+" "
+            temp=""
+        return binary
 
-print(pizza.text_to_bits(text="Hello"))
+print(pizza.text_to_bits(text="Hello World!"))
